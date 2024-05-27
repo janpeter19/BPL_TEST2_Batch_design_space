c17cd3320019cbf70853d5fdde15382e16cc9e9c
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2024-05-27 21:05:58 +0200

    Update BPL_TEST2_Batch_no_noise_explore.py
    Spelling

--- BPL_TEST2_Batch_no_noise_explore.py
+++ BPL_TEST2_Batch_no_noise_explore.py
@@ -102,7 +102,7 @@ elif platform.system() == 'Linux':
    flag_vendor = 'OM'
    flag_type = 'ME'
    if flag_vendor in ['OM','om']:
-      print('Linux - run FMU pre-comiled OpenModelica 1.23.0-dev') 
+      print('Linux - run FMU pre-compiled OpenModelica 1.23.0-dev') 
       if flag_type in ['CS','cs']:         
          fmu_model ='BPL_TEST2_BatchNoNoise_linux_om_cs.fmu'    
          model = load_fmu(fmu_model, log_level=0) 
