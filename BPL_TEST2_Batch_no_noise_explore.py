7d2a49cf0a6c623cd2280f170d26001b4312a68a
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2026-03-31 22:26:54 +0200

    Update FMU-explore 1.0.3 and FMUs recompiled now including description

--- BPL_TEST2_Batch_no_noise_explore.py
+++ BPL_TEST2_Batch_no_noise_explore.py
@@ -69,6 +69,7 @@
 # 2025-11-09 - FMU-explore 1.0.2
 # 2025-11-14 - FMU-explore 1.0.2 corrected
 # 2025-11-19 - FMU-explore 1.0.2 corrected again parLocation() with sheets as argument
+# 2026-03-31 - FMU-explore 1.0.3
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -337,7 +338,7 @@ def describe(name, decimals=3):
       
 #------------------------------------------------------------------------------------------------------------------
 #  General code 
-FMU_explore = 'FMU-explore version 1.0.2'
+FMU_explore = 'FMU-explore version 1.0.3'
 #------------------------------------------------------------------------------------------------------------------
 
 # Define function par() for parameter update
@@ -579,6 +580,9 @@ def describe_general(name, decimals, parLocation=parLocation):
       unit = 'h'
       print(description,'[',unit,']')
       
+   elif name == 'process':
+      print(model.get_description())    
+      
    elif name in parLocation.keys():
       description = model.get_variable_description(parLocation[name])
       value = model.get(parLocation[name])[0]
@@ -662,6 +666,13 @@ def system_info():
    print(' -MSL:', MSL_version)    
    print(' -Description:', BPL_version)   
    print(' -Interaction:', FMU_explore)
+   
+def SDG(explanation=False):
+  if explanation:
+    print('"Soli Deo Gloria"')
+    print(' It is latin and means "To the honour of God".') 
+    print(' The great composer Johan Sebastian Bach used to end his compositions with this small remark SDG.')
+    print(' And I like to do that too :).')    
    
 #------------------------------------------------------------------------------------------------------------------
 #  Startup
--- BPL_TEST2_Batch_no_noise_fmpy_explore.py
+++ BPL_TEST2_Batch_no_noise_fmpy_explore.py
@@ -74,6 +74,7 @@
 # 2025-11-10 - Update FMU-explore 1.0.2
 # 2025-11-16 - FMU-explore 1.0.2 corrected
 # 2025-11-19 - FMU-explore 1.0.2 corrected again parLocation() with sheets as argument
+# 2026-03-31 - FMU-explore 1.0.3
 #------------------------------------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------------------------------------
@@ -376,7 +377,7 @@ def describe(name, decimals=3):
       
 #------------------------------------------------------------------------------------------------------------------
 #  General code 
-FMU_explore = 'FMU-explore for FMPy version 1.0.2'
+FMU_explore = 'FMU-explore for FMPy version 1.0.3'
 #------------------------------------------------------------------------------------------------------------------
 
 # Define function par() for parameter update
@@ -667,6 +668,9 @@ def describe_general(name, decimals, parLocation=parLocation):
       description = 'Time'
       unit = 'h'
       print(description,'[',unit,']')
+
+   elif name == 'process':
+      print(read_model_description(fmu_model).description)   
       
    elif name in parLocation.keys():
       description = model_get_variable_description(parLocation[name])
@@ -756,6 +760,13 @@ def system_info():
    print(' -MSL:', MSL_version)    
    print(' -Description:', BPL_version)   
    print(' -Interaction:', FMU_explore)
+   
+def SDG(explanation=False):
+  if explanation:
+    print('"Soli Deo Gloria"')
+    print(' It is latin and means "To the honour of God".') 
+    print(' The great composer Johan Sebastian Bach used to end his compositions with this small remark SDG.')
+    print(' And I like to do that too :).')    
    
 #------------------------------------------------------------------------------------------------------------------
 #  Startup
